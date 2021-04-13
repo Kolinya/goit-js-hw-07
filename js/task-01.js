39b6030e7b983babc9057c312f921120c1a33328
Kolinya <74129726+Kolinya@users.noreply.github.com>
2021-04-13 21:19:19 +0300

    add 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const itemRef = document.querySelectorAll('.js-item');
-console.log(`В списке ${listRef.length} категории`);
+const itemRef = document.querySelectorAll(".js-item");
+console.log(`В списке ${itemRef.length} категории`);
 
-itemRef.forEach(item => {
-    console.log(`Категория: ${item.firstElementChild.textContent}`);
-    console.log(`Количество элементов: ${item.querySelectorAll('li').length}`);
-})
+itemRef.forEach((item) => {
+  console.log(`Категория: ${item.firstElementChild.textContent}`);
+  console.log(`Количество элементов: ${item.querySelectorAll("li").length}`);
+});
